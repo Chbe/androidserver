853954a6d293cc2359cf276e8521749505ebda16
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2017-09-16 03:01:19 +0200

    headhers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Origin:  http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://androidserverapp.herokuapp.com/*');
+  res.header('Access-Control-Allow-Origin', 'https://androidserverapp.herokuapp.com/*/*');
+  res.header('Access-Control-Allow-Origin', 'http://pinechat.azurewebsites.net');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   // intercept OPTIONS method
