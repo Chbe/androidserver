5a5315418eb81128048c04f742b9489ffdde9674
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2017-09-16 16:21:44 +0200

    cors header

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', request.get('origin'));
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Origin', 'https://androidserverapp.herokuapp.com/*');
